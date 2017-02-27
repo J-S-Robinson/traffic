fb63638c51cf7976ca7167be03840a71e6671da5
Robinson <jrobinson47@gatech.edu>
2017-02-27 17:37:17 -0500

    Add Utils

---
+++ utils/network.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 18 23:49:57 2017
+
+@author: Harvey
+"""
+import xml.etree.ElementTree as ET
+from subprocess import Popen
+
+class Node: 
+    
+    def __init__(self,ID,x,y,isTL=False):
+        self.ID = ID
+        self.x = x
+        self.y = y
+        self.isTL = isTL
+
+class Edge:
+    
+    def __init__(self,start,end,numLanes=1,speed=15):
+        #start and end are Nodes
+        self.startNode = start
+        self.endNode = end
+        self.numLanes = numLanes
+        self.speed = speed
+        self.ID = '_'.join((self.startNode.ID,self.endNode.ID))
+        
+class Network:
+    
+    def __init__(self,nodes,edges):
+        self.nodes = nodes
+        self.edges = edges
+        
+    def _genEdgeFile(self,out):
+        edgeRoot = ET.Element('edges')
+        for edge in self.edges:
+            new_edge = ET.Element('edge',
+                                  attrib = {'from':edge.startNode.ID,
+                                            'to':edge.endNode.ID,
+                                            'numLanes':'%i' % edge.numLanes,
+                                            'speed':'%i' % edge.speed,
+                                            'id':edge.ID})
+            edgeRoot.append(new_edge)
+        edgeTree = ET.ElementTree()
+        edgeTree._setroot(edgeRoot)
+        edgeTree.write(out)
+        
+    def _genNodeFile(self,out):
+        nodeRoot = ET.Element('nodes')
+        for node in self.nodes:
+            new_node = ET.Element('node',
+                                  attrib = {'x':'%i' % node.x,
+                                            'y':'%i' % node.y,
+                                            'id':node.ID})
+            if node.isTL:
+                new_node.set('type','traffic_light')
+            nodeRoot.append(new_node)
+        nodeTree = ET.ElementTree()
+        nodeTree._setroot(nodeRoot)
+        nodeTree.write(out)
+        
+    def _genConNoUturn(self,out):
+        conRoot = ET.Element('connections')
+        for edge1 in self.edges:
+            for edge2 in self.edges:
+                if (edge1.endNode == edge2.startNode and
+                        edge1.startNode != edge2.endNode):
+                    new_con=ET.Element('connection',
+                                       attrib = {'from':edge1.ID,
+                                                 'to':edge2.ID})
+                    conRoot.append(new_con)
+        conTree = ET.ElementTree()
+        conTree._setroot(conRoot)
+        conTree.write(out)
+        
+    def writeNet(self,sumo_path,nameroot):
+        self._genNodeFile('%s.nod.xml' % nameroot)
+        self._genEdgeFile('%s.edg.xml' % nameroot)
+        self._genConNoUturn('%s.con.xml' % nameroot)
+        Popen(['%s\\netconvert.exe' % sumo_path,
+              '-n','%s.nod.xml' % nameroot,
+              '-e','%s.edg.xml' % nameroot,
+              '-x', '%s.con.xml' % nameroot,
+              '-o', '%s.net.xml' % nameroot])
+
+
+    
+    
---
+++ utils/traffic.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 12:19:08 2017
+
+@author: jrobinson47
+"""
+
+from numpy import random
+import xml.etree.ElementTree as ET
+
+class VehicleType:
+    
+    def __init__(self,color,ID):
+        self.color = color
+        self.ID = ID
+        
+    def get_XML_Element(self):
+        return ET.Element('vType', 
+                   attrib={'color':','.join(str(x) for x in self.color),
+                           'id':self.ID})
+        
+class Vehicle:
+    
+    def __init__(self,ID,departTime,vType,route,departSpeed=15,departLane='free'):
+        self.ID = ID
+        self.departTime = departTime
+        self.departSpeed = departSpeed
+        self.departLane = departLane
+        self.route = route
+        self.vType = vType
+        
+    def get_XML_Element(self):
+        vehElem = ET.Element('vehicle',
+                          attrib={'id':self.ID,
+                                  'depart':'%s' % self.departTime,
+                                  'departLane': '%s' % self.departLane,
+                                  'departSpeed': '%s' % self.departSpeed,
+                                  'type':self.vType.ID})
+        vehElem.append(self.route.get_XML_Element())
+        return vehElem
+
+class Route:
+    
+    def __init__(self,edges):
+        self.edges = edges
+        
+    def get_XML_Element(self):
+        return ET.Element('route',
+                          attrib={'edges': ' '.join(self.edges)})
+        
+class Demand:
+    
+    def __init__(self,vTypes,vehicles):
+        self.vTypes = vTypes
+        self.vehicles = vehicles
+    
+    def _def_rand_departs(n, avg_interval, min_interval=False):
+        t = [None] * n
+        t[0] = random.exponential(scale=avg_interval)
+        for i in range(1,n):
+            interval = random.exponential(scale=avg_interval)
+            if min_interval:
+                if interval < min_interval:
+                    interval = min_interval
+            t[i] = t[i-1] + interval
+        return t
+        
+    def genRouteFile(self,out):
+        routeRoot = ET.Element('routes')
+        for vType in self.vTypes:
+            new_vType = vType.get_XML_Element()
+            routeRoot.append(new_vType)
+        for vehicle in self.vehicles:
+            new_veh = vehicle.get_XML_Element()
+            routeRoot.append(new_veh)
+        routeTree = ET.ElementTree()
+        routeTree._setroot(routeRoot)
+        routeTree.write(out)
