180d2d5411d14d2a3770677e704b8c32c64fedcc
Robinson <jrobinson47@gatech.edu>
2017-03-21 17:11:23 -0400

    Continued development of DUAGawron

--- DUAGawron.py
+++ DUAGawron.py
@@ -11,93 +11,71 @@ Created on Mon Mar 20 15:04:31 2017
 # is modified from SUMO's built in duaIterate.py to enable loading the traffic
 # state from a state file, essentially starting the routing mid-simulation 
 #==============================================================================
+import subprocess
+
 def runDUAIteratepartial():
     pass
 
-def runSUMO(step):
+def runSUMO(step, netFile, routeFile, loadState = False, stateFile = '', begin=0):
     # First Generate Configuration for SUMO
     
     # Create Output File Definition
-    outputFile = 'edgeData.out.xml'
+    outputFile = 'data\\edgeData.out.xml'
+    
+    # Create output configuration
     # Set period over which edge data is aggregated
-    aggregation = 900;
+    aggregation = 900
     
+    addFile = 'data\\outputDef.xml'
     
-    def writeSUMOConf(sumoBinary, step, options, additional_args, route_files,
-                  state_file = None):
-    detectorfile = "dua_dump_%03i.add.xml" % step
-    comma = (',' if options.additional != "" else '')
-    sumoCmd = [sumoBinary,
+    with open(addFile, 'w') as fd:
+        print('<a>', file=fd)
+        print('   <edgeData id="dump%s" freq="%s" file="%s" excludeEmpty="true" minSamples="1"/>'
+              % (step, aggregation, outputFile), file=fd)
+        print('</a>', file=fd)    
+    
+    sumoCmd = [sumoBinary, 
                '--save-configuration', "iteration_%03i.sumocfg" % step,
-               '--net-file', options.net,
-               '--route-files', route_files,
-               '--additional-files', "%s%s%s" % (
-                   detectorfile, comma, options.additional),
+               '--net-file', netFile,
+               '--route-files',routeFile,
+               '--additional-files', addFile,
+               '--seed', '%s' % 12345,
                '--no-step-log',
-               '--random', options.absrand,
-               '--begin', options.begin,
-               '--route-steps', options.routeSteps,
-               '--no-internal-links', options.internallink,
-               '--eager-insert', options.eager_insert,
                '--verbose',
-               '--no-warnings', options.noWarnings,
-               ] + additional_args
-
-    if hasattr(options, "noSummary") and not options.noSummary:
-        sumoCmd += ['--summary-output', "summary_%03i.xml" % step]
-    if hasattr(options, "noTripinfo") and not options.noTripinfo:
-        sumoCmd += ['--tripinfo-output', "tripinfo_%03i.xml" % step]
-        if options.ecomeasure:
-            sumoCmd += ['--device.hbefa.probability', '1']
-    if hasattr(options, "routefile"):
-        if options.routefile == "routesonly":
-            sumoCmd += ['--vehroute-output', "vehroute_%03i.xml" % step,
-                        '--vehroute-output.route-length']
-        elif options.routefile == "detailed":
-            sumoCmd += ['--vehroute-output', "vehroute_%03i.xml" % step,
-                        '--vehroute-output.route-length',
-                        '--vehroute-output.exit-times']
-    if hasattr(options, "lastroute") and options.lastroute:
-        sumoCmd += ['--vehroute-output.last-route', options.lastroute]
-    if hasattr(options, "timeInc") and options.timeInc:
-        sumoCmd += ['--end', int(options.timeInc * (step + 1))]
-    elif options.end:
-        sumoCmd += ['--end', options.end]
-
-    if hasattr(options, "incBase") and options.incBase > 0:
-        sumoCmd += ['--scale', get_scale(options, step)]
-    if options.mesosim:
-        sumoCmd += ['--mesosim',
-                    '--meso-recheck', options.mesorecheck]
-        if options.mesomultiqueue:
-            sumoCmd += ['--meso-multi-queue']
-        if options.mesojunctioncontrol:
-            sumoCmd += ['--meso-junction-control']
+               '--begin','%s' % begin]
+    if loadState:
+       sumoCmd += ['--load-state', stateFile]
     
-    # John -- make sure it starts with exact initial conditions
-    if state_file != None:
-         sumoCmd += ["--load-state",state_file]
     # make sure all arguments are strings
     sumoCmd = list(map(str, sumoCmd))
     # use sumoBinary to write a config file
     subprocess.call(sumoCmd, stdout=subprocess.PIPE)
 
-    # write detectorfile
-    with open(detectorfile, 'w') as fd:
-        suffix = "_%03i_%s" % (step, options.aggregation)
-        print("<a>", file=fd)
-        if options.costmodifier != 'None':
-            print('    <edgeData id="dump%s" freq="%s" file="%s" excludeEmpty="defaults" minSamples="1"/>' % (
-                suffix, options.aggregation, get_dumpfilename(options, step, "dump")), file=fd)
-        else:
-            print('    <edgeData id="dump%s" freq="%s" file="%s" excludeEmpty="true" minSamples="1"/>' % (
-                suffix, options.aggregation, get_dumpfilename(options, step, "dump")), file=fd)
-        if options.ecomeasure:
-            print('    <edgeData id="eco%s" type="hbefa" freq="%s" file="dump%s.xml" excludeEmpty="true" minSamples="1"/>' %
-                  (suffix, options.aggregation, suffix), file=fd)
-        print("</a>", file=fd)
     
-    pass
+def run DUARouter(step, netFile, inputFile, outputFile, weightFile='', begin=0):
+    routerCmd = [duarouterBinary,
+                 '--net-file', netFile,
+                 '--output-file', outputFile,
+                 '--verbose', 'true',
+                 '--gawron.a', '0.05',
+                 '--gawron.beta', '0.3',
+                 '--max-alternatives', '5',
+                 '--weights.expand', 'true',
+                 '--seed', '%s' % 12345,
+                 '--begin', '%' % begin]
+    if step == 0:
+        routerCmd += ['--route-files', inputFile]
+    else:
+        routerCmd += ['--alternative-files', inputFile,
+                      '--weight-files', weightFile]
+                 
+    def writeRouteConf(duarouterBinary, step, options, dua_args, file,
+                   output, routesInfo, initial_type):
+
+    cfgname = "iteration_%03i_router.duarcfg" % step
+    subprocess.call(
+        [duarouterBinary, "--save-configuration", cfgname])
+    return cfgname
 
 def runDUAGawron(netfile,routefile,numIters=25):
     # for each time step, first run DUAITERATE and then SUMO
@@ -111,4 +89,4 @@ def runDUAGawron(netfile,routefile,numIters=25):
         # SUMO also needs a route and network file as inputs it will output the
         # travel time on each edge, which will be used by the next iteration of
         # DUAITERATE
-        runSUMO()
+        runSUMO(i, netfile, routefile)
